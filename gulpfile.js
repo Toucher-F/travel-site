fb4396adfa0030667b2421e83145b555155546a7
Toucher-F <lianduanb@gmail.com>
2018-05-02 17:44:27 -0400

    add browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-var gulp = require("gulp");
-var watch = require("gulp-watch");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var cssvars = require("postcss-simple-vars");
-var nested = require("postcss-nested");
-var cssImport = require('postcss-import');
+var gulp = require("gulp"),
+watch = require("gulp-watch"),
+postcss = require("gulp-postcss"),
+autoprefixer = require("autoprefixer"),
+cssvars = require("postcss-simple-vars"),
+nested = require("postcss-nested"),
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 gulp.task("default", function() {
   console.log("you created a Gulp task");
@@ -22,11 +23,24 @@ gulp.task("styles", () => {
 });
 
 gulp.task("watch", () => {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
   watch("./app/index.html", () => {
-    gulp.start("html");
+    browserSync.reload();
   });
 
   watch("./app/assets/styles/**/*.css", () => {
-    gulp.start("styles");
+    gulp.start("cssInject");
   });
+});
+
+gulp.task("cssInject", ["styles"], () => {
+  return gulp.src("./app/temp/styles/styles.css")
+    .pipe(browserSync.stream());
 });
